fd77a4ffcc9abc7994d5b81a20b0fad58790eed5
Mohammed ElSaady <m.elsaady@trips.sa.com>
2026-03-23 22:27:48 +0200

    Add OrderCreatedIntegrationEvent and domain reference
    Added a project reference to Evently.Common.Domain to enable use of shared event interfaces. Introduced OrderCreatedIntegrationEvent record implementing IEvent, encapsulating order creation details for integration purposes.

---
+++ Modules/Ticketing/Evently.Modules.Ticketing.PublicApi/OrderCreatedIntegrationEvent.cs
@@ -0,0 +1,6 @@
+﻿using Evently.Common.Domain.Events;
+
+namespace Evently.Modules.Ticketing.PublicApi;
+
+public sealed record OrderCreatedIntegrationEvent(Guid OrderId, Guid CustomerId, Guid EventId, decimal TotalPrice) : IEvent;
+
